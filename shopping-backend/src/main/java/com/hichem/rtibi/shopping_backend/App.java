db1b1bc620c437176bbb90a16ec0e0dbc5f6a868
HichemRtibi <hichem.rtibi@gmail.com>
2018-06-03 15:02:10 +0200

    My project Setup

---
+++ shopping-backend/src/main/java/com/hichem/rtibi/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package com.hichem.rtibi.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
